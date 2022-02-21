fb12c82798a93cd73cfe67c3c3667144684488fa
Scott Hillier <scotthhillier@gmail.com>
2022-02-21 14:36:52 -0800

    formatting the front page

--- polo-react/src/components/Home.js
+++ polo-react/src/components/Home.js
@@ -1,18 +1,28 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../styles/Home.scss";
 
 const Home = () => {
+  const search = (string) => {};
+
   return (
     <main>
       <section className="search">
-        <input placeholder="Search by tournament"></input>
-        <button>Search</button>
+        <input
+          className="search-bar"
+          placeholder="Search by tournament"
+          size={"25"}
+        ></input>
+        <button className="search-button">Find!</button>
       </section>
       <section className="organize">
         <h2>Want to organize a tournament now?</h2>
-        <Link to={`/organize`} type="button" className="organize">
+        <Link to={`/organize`} type="button" className="organize-button">
           Organize
         </Link>
+      </section>
+      <section className="upcoming-tournaments">
+        <h2>Tournaments happening soon!</h2>
       </section>
     </main>
   );
