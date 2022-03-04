7741d35f15ea2b530c9a10a36eb229785d6d2dc4
Scott Hillier <scotthhillier@gmail.com>
2022-03-04 08:57:48 -0800

    added a function

--- polo-react/src/helpers/apiHelpers.js
+++ polo-react/src/helpers/apiHelpers.js
@@ -9,6 +9,10 @@ export function searchTournaments(word) {
   return axios.get("api/tournaments/search");
 }
 
-export function createTournament() {
+export function createTournament(user_id) {
   return axios.put("api/tournament/create");
+}
+
+export function createTeam(team_name) {
+  return axios.put("api/teams/create");
 }
