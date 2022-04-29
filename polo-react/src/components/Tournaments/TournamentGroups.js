228edef1d66c3052974a5b7cc41adda58be4f0ab
Scott Hillier <scotthhillier@gmail.com>
2022-04-29 14:55:02 -0700

    groups now displayed better for clarity

--- polo-react/src/components/Tournaments/TournamentGroups.js
+++ polo-react/src/components/Tournaments/TournamentGroups.js
@@ -1,14 +1,17 @@
 import React, { useEffect, useState } from "react";
 import TournamentTeams from "./TournamentTeams";
+import "../../styles/Tournaments/TournamentGroups.scss";
 
 const TournamentGroups = ({ group }) => {
   console.log(group);
   return (
-    <div>
+    <div className="groups">
       <h1>Group</h1>
-      {group.map((team) => {
-        return <TournamentTeams key={team.id} team={team} />;
-      })}
+      <div className="group">
+        {group.map((team) => {
+          return <TournamentTeams key={team.id} team={team} />;
+        })}
+      </div>
     </div>
   );
 };
