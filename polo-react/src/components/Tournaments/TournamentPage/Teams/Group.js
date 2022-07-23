c80638ca4bdf1a666ca86fcc05dfd479699a59e1
Scott Hillier <scotthhillier@gmail.com>
2022-07-23 16:22:39 -0700

    .map function now working with no errors

--- polo-react/src/components/Tournaments/TournamentPage/Teams/Group.js
+++ polo-react/src/components/Tournaments/TournamentPage/Teams/Group.js
@@ -4,8 +4,8 @@ import Team from "./Team";
 const Group = ({ group, tournament_id }) => {
   return group.map((team, i) => {
     return (
-      <div className="team">
-        <Team key={i} team={team} tournament_id={tournament_id} />
+      <div className="team" key={i}>
+        <Team team={team} tournament_id={tournament_id} />
       </div>
     );
   });
--- polo-react/src/components/Tournaments/TournamentPage/Teams/Teams.js
+++ polo-react/src/components/Tournaments/TournamentPage/Teams/Teams.js
@@ -6,16 +6,16 @@ const Teams = ({ teams, groups, format, tournament_id }) => {
   return format === "Round Robin"
     ? groups.map((group, i) => {
         return (
-          <section className="groups">
+          <section className="groups" key={i}>
             <h1>GROUP {group[0].group_id}</h1>
-            <Group key={i} group={group} tournament_id={tournament_id} />
+            <Group group={group} tournament_id={tournament_id} />
           </section>
         );
       })
     : teams.map((team, i) => {
         return (
-          <section className="team">
-            <Team key={i} team={team} tournament_id={tournament_id} />
+          <section className="team" key={i}>
+            <Team team={team} tournament_id={tournament_id} />
           </section>
         );
       });
