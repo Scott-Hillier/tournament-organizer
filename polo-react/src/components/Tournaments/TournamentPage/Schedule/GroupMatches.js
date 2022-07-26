375a149154fe85443fbf26e21f0c52f9633d2d90
Scott Hillier <scotthhillier@gmail.com>
2022-07-26 11:46:46 -0700

    changed formatting for round robin v swiss rounds in the scheudle

--- polo-react/src/components/Tournaments/TournamentPage/Schedule/GroupMatches.js
+++ polo-react/src/components/Tournaments/TournamentPage/Schedule/GroupMatches.js
@@ -1,10 +1,14 @@
 import React from "react";
 import Match from "./Match";
 
-const GroupMatches = ({ group }) => {
+const GroupMatches = ({ group, format }) => {
   return (
     <section className="schedule-groups">
-      <h2>Group Matches</h2>
+      {format === "Round Robin" ? (
+        <h2>Group Matches</h2>
+      ) : (
+        <h2>Round {group[0].round_id}</h2>
+      )}
       {group.map((match, i) => {
         return <Match key={i} match={match} />;
       })}
--- polo-react/src/components/Tournaments/TournamentPage/Schedule/Schedule.js
+++ polo-react/src/components/Tournaments/TournamentPage/Schedule/Schedule.js
@@ -3,22 +3,14 @@ import GroupMatches from "./GroupMatches";
 import Match from "./Match";
 
 const Schedule = ({ schedule, teams, groups, format }) => {
-  return format === "Round Robin"
-    ? schedule.map((group, i) => {
-        return (
-          <section className="groups">
-            <h1>GROUP {group[0].group_id}</h1>
-            <GroupMatches key={i} group={group} />
-          </section>
-        );
-      })
-    : schedule.map((match, i) => {
-        return (
-          <section className="team">
-            <Match key={i} match={match} />
-          </section>
-        );
-      });
+  return schedule.map((group, i) => {
+    return (
+      <section className="groups" key={i}>
+        {format === "Round Robin" && <h1>GROUP {group[0].group_id}</h1>}
+        <GroupMatches group={group} format={format} />
+      </section>
+    );
+  });
 };
 
 export default Schedule;
