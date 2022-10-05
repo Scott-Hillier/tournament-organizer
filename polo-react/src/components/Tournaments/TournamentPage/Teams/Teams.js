7b90c540693e68aa3b77d8748a39a7a93802d930
Scott-Hillier <scotthhillier@gmail.com>
2022-10-05 11:45:39 -0700

    refactoring code for legibility

--- polo-react/src/components/Tournaments/TournamentPage/Teams/Teams.js
+++ polo-react/src/components/Tournaments/TournamentPage/Teams/Teams.js
@@ -10,7 +10,17 @@ const Teams = ({ teams, groups, format, tournament_id }) => {
             <section className="groups-header" key={i}>
               <h1>GROUP {group[0].group_id}</h1>
               <div className="groups">
-                <Group group={group} tournament_id={tournament_id} />
+                {group.map((team, i) => {
+                  return (
+                    <section className="team" key={i}>
+                      <Team
+                        team={team}
+                        tournament_id={tournament_id}
+                        // removeTeam={removeTeam}
+                      />
+                    </section>
+                  );
+                })}
               </div>
             </section>
           )
