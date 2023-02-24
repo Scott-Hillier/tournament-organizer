ead99b9e9ac9a276d5ffc42025aa1385bffc8296
Scott-Hillier <scotthhillier@gmail.com>
2023-02-24 12:54:06 -0800

    qwe

--- react-frontend/src/components/tournament/components/Standings.js
+++ react-frontend/src/components/tournament/components/Standings.js
@@ -3,25 +3,29 @@ const Standings = ({ groupOrder, teams, groups }) => {
     return teams[b].wins - teams[a].wins;
   };
   return (
-    <>
-      <div>STANDINGS</div>
-      <div className="flex flex-wrap justify-center">
-        {groupOrder.map((groupId) => {
-          return (
-            <div key={groupId} className="flex flex-col">
-              {console.log(groups[groupId].teamIds.sort(sortByWins))}
-              {groups[groupId].teamIds.sort(sortByWins).map((teamId) => {
-                return (
-                  <div>
-                    {teams[teamId].name} {teams[teamId].wins}
-                  </div>
-                );
-              })}
+    <div className="flex flex-wrap w-full justify-center my-4">
+      {groupOrder.map((groupId) => {
+        return (
+          <div
+            key={groupId}
+            className="flex flex-col w-64 mx-20 m-2 p-1 border-2 border-black rounded"
+          >
+            <div className="flex justify-between p-1 border-b border-black font-bold">
+              <p>Team</p>
+              <p>Wins</p>
             </div>
-          );
-        })}
-      </div>
-    </>
+            {groups[groupId].teamIds.sort(sortByWins).map((teamId) => {
+              return (
+                <div className="flex justify-between p-1 break-all border-b">
+                  <p className="w-40">{teams[teamId].name}</p>
+                  <p>{teams[teamId].wins}</p>
+                </div>
+              );
+            })}
+          </div>
+        );
+      })}
+    </div>
   );
 };
 
